7a85afa06a5868c6dd1249985b91f899826c088c
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2024-12-14 13:09:23 +0530

    Day 1

---
+++ 01. Working with Variables in Python to Manage Data/1. printing/main.py
@@ -0,0 +1,12 @@
+print("Hello World") 
+
+print("Hello World\nHello World") 
+
+print("Hello " + " Kasun") 
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello "+"world")')
+print("New lines can be created with a backlash and n.")
+
+print("Hello " + input("What is yout name? "))
