db537375c75b31021fa4d02422a66f4fdd4c3742
Dinusha Kasun Heenatiyangala <dinushakasun1234@gmail.com>
2025-02-18 00:22:38 +0530

    Add randomisation examples and list manipulation exercises for Day 4

---
+++ 04. Randomisation and Python Lists/1. Random Module/main.py
@@ -0,0 +1,22 @@
+import random
+import my_module
+
+# Random float between 0 and 1
+random_integer = random.randint(1, 10)
+print(random_integer)
+
+# Print the value of pi from my_module
+print(my_module.pi)
+
+# Random float between 0 and 1
+random_float = random.random() 
+print(random_float)
+
+# Random float between 0 and 5
+random_float = random.random() * 5
+
+# Random love score
+love_score = random.randint(1, 100)
+print(f"Your love score is {love_score}")
+
+
---
+++ 04. Randomisation and Python Lists/3. Understanding the Offset and Appending Items to Lists/main.py
@@ -0,0 +1,11 @@
+# Lists in Python are defined by square brackets [] and can contain any data type or a mix of data types. 
+states_of_america = ["Delaware", "Pennsylvania", "New Jersey", "Georgia", "Connecticut", "Massachusetts", "Maryland", "South Carolina", "New Hampshire", "Virginia", "New York", "North Carolina", "Rhode Island", "Vermont", "Kentucky", "Tennessee", "Ohio", "Louisiana", "Indiana", "Mississippi", "Illinois", "Alabama", "Maine", "Missouri", "Arkansas", "Michigan", "Florida", "Texas", "Iowa", "Wisconsin", "California", "Minnesota", "Oregon", "Kansas", "West Virginia", "Nevada", "Nebraska", "Colorado", "North Dakota", "South Dakota", "Montana", "Washington", "Idaho", "Wyoming", "Utah", "Oklahoma", "New Mexico", "Arizona", "Alaska", "Hawaii"]
+
+print(states_of_america[2]) 
+
+# Last item in the list
+print(states_of_america[-1]) 
+
+# Add an item to the end of the list
+states_of_america.append("Dinusha")
+print(states_of_america)
